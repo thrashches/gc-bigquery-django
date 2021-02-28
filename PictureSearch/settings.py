76cb8cfed466937610a9acbf0add86da53e16df3
thrashches <thrashches@yandex.ru>
2021-02-28 19:38:08 +0300

    Add app.yaml

--- PictureSearch/settings.py
+++ PictureSearch/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '_r56$ba2!rf5rs5--!h(qix=q3c-)sv*%##!nx=)h5loljr)s)'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -85,8 +85,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = ''
-STATICFILES_DIRS = (os.path.join('static'),)
+STATIC_ROOT = 'static'
+#STATICFILES_DIRS = (os.path.join('static'),)
 
 DEFAULT_FILE_STORAGE = 'storages.backends.gcloud.GoogleCloudStorage'
 GS_BUCKET_NAME = 'picture_search'
